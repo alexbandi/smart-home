c8478f311ad7b9598efd96b04d4d87c33a217695
Alexander Bandhauer <alexbandhauer@outlook.com>
2024-07-07 08:59:05 +0200

    Replace Log class with ArduinoLog

--- sensors/air-temperature-humidity/src/main.cpp
+++ sensors/air-temperature-humidity/src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
+#include <ArduinoLog.h>
 #include "sensors/Sht35.h"
-#include "utils/Log.h"
 
 void setup()
 {
-    Log::init();
+    Serial.begin(115200);
+
+    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 
     if (!Sht35::init())
     {
-        Log::error("Could not find the SHT35 sensor. Make sure it has been connected properly.");
+        Log.fatalln("Could not find the SHT35 sensor. Make sure it has been connected properly.");
         while (true)
             delay(10);
     }
@@ -19,10 +21,10 @@ void loop()
     float temp;
     float humidity;
     if (Sht35::getBoth(&temp, &humidity)) {
-        Log::info("Temperature: %.2f °C", temp);
-        Log::info("Humidity: %.2f %", humidity);
+        Log.infoln("Temperature: %.2f °C", temp);
+        Log.infoln("Humidity: %.2f %", humidity);
     } else {
-        Log::error("Could not read temperature/humidity.");
+        Log.errorln("Could not read temperature/humidity.");
     }
 
     delay(1000);
